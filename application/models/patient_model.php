554396565f68bb30c64479a47e15cfe6f94793c1
Shao <shaohuan.li@gmail.com>
2013-07-30 13:53:03 +0800

    Replaced the CCDA.json Location

--- application/models/patient_model.php
+++ application/models/patient_model.php
@@ -7,7 +7,7 @@ class Patient_model extends CI_Model {
     }
     function get_record($uid)
     {
-        $record = file_get_contents("http://localhost/~shaohuanli/ccda.json");
+        $record = file_get_contents(getcwd()."/ccda.json");
         return json_decode($record);
     }
 
